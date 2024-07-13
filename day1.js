fddab1a245d696f985fa4e888fe60ad9b277933b
nidhi-27 <46447687+nidhi-27@users.noreply.github.com>
2024-07-13 14:54:50 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,45 @@
+// Variable Declaration
+var num1 = 5
+console.log(num1)
+
+let str1 = "Nidhi"
+console.log(str1) 
+
+// Constant declaration
+const bln = true
+console.log(bln)
+
+// Data types
+console.log(typeof(num1))
+console.log(typeof(str1))
+console.log(typeof(bln))
+// object - ?
+// array - ?
+let obj = {name: "Nidhi",age: 25}
+console.log(typeof(obj))
+let arr = ["Nidhi","Neha", 26, {name: "Trisha",age: 10}, true ]
+console.log(typeof(arr))
+
+//Reassigning Variables
+let num = 65
+console.log(num)
+num = 55
+console.log(num)
+
+// Understanding constant
+// const num2 = 65
+// console.log(num2)
+// num2 = 55
+// console.log(num2)
+
+// Feature Request
+var name = "Nidhi"
+var age = 26
+var girl = true
+
+console.log(name)
+console.log(typeof(name))
+console.log(age)
+console.log(typeof(age))
+console.log(girl)
+console.log(typeof(girl))
